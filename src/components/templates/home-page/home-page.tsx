e2aba161c3d175732212578c885d6db4ba28924f
Kryštof Matějka <krystof.m@pm.me>
2021-07-18 18:46:43 +0200

    feat: improve container of home page

--- src/components/templates/home-page/home-page.tsx
+++ src/components/templates/home-page/home-page.tsx
@@ -1,13 +1,19 @@
 import {FC, useState} from 'react'
 import styled from 'styled-components'
 import {Filter, Mockups} from '../../organisms'
+import {BREAKPOINTS} from '../../../theme'
 
 const StyledFilter = styled(Filter)`
   margin-bottom: 20px;
 `
 
 const Container = styled.div`
+  max-width: 1055px;
+  margin: 30px;
 
+  @media (max-width: ${BREAKPOINTS.M}px) { {
+    margin: 15px;
+  }
 `
 
 type Filter = {
