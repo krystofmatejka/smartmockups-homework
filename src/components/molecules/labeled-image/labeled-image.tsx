95b5c187fcfd6fa96878de9dedb5565850800494
Kryštof Matějka <krystof.m@pm.me>
2021-07-18 09:00:44 +0200

    fix: add border box to image

--- src/components/molecules/labeled-image/labeled-image.tsx
+++ src/components/molecules/labeled-image/labeled-image.tsx
@@ -10,19 +10,16 @@ const Container = styled.div`
 const HoverArea = styled.div`
   display: flex;
   align-items: end;
-
   transition: opacity 0.3s ease;
   opacity: 0;
-
   position: absolute;
   top: 0;
   left: 0;
-
   width: 100%;
   height: 100%;
   border-radius: 4px;
   border: 1px solid rgba(17, 190, 255, 0);
-
+  box-sizing: border-box;
   background: linear-gradient(180deg, rgba(74, 74, 74, 0) 0%, rgba(0, 0, 0, 0.69) 100%);
 
   &:hover {
@@ -47,7 +44,7 @@ export const LabeledImage: FC<LabeledImageProps> = ({label, image, className}) =
       <HoverArea>
         <StyledText color='neutral-100'>{label}</StyledText>
       </HoverArea>
-      <Image src={image} alt={label}  />
+      <Image src={image} alt={label} />
     </Container>
   )
 }
